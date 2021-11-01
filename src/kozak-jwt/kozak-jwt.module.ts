cb6e333cc7c9d8eb0983d8a4d2cb9656e441fa8f
kozak201201 <kozak201201@gmail.com>
2021-11-01 22:36:53 +0200

    add dependencies

--- src/kozak-jwt/kozak-jwt.module.ts
+++ src/kozak-jwt/kozak-jwt.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { KozakJwtService } from './kozak-jwt.service';
 import { KozakJwtModel } from './kozak-jwt.model';
 import { SequelizeModule } from '@nestjs/sequelize';
